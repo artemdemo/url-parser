78610e49f04f8d555afebcd5963e2fe0e5a83278
Artem Demochko <artem@mycheck.co.il>
2015-03-31 11:03:26 +0300

    Added JS

---
+++ url-parser.js
@@ -0,0 +1,23 @@
+var UrlParser =
+	(function(){
+		"use strict";
+
+		var UrlParser = {};
+
+		/**
+		 * Return value of given key<br>
+		 * Will return string if value for given key exists or null if not
+		 * 
+		 * @param key {String}
+		 * @return {String || null}
+		 */
+		UrlParser.getParam = function( key ) {
+			var match = RegExp('[?&]' + key + '=([^&]*)').exec(window.location.href);
+            return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+		}
+
+		return UrlParser;
+
+	})();
+
+console.log( UrlParser.getParam('appId') );
